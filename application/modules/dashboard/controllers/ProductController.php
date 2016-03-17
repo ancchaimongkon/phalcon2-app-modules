711a7ec5c04eef30d0de265c6317983e91528411
coachmaxz <eakkabin_it@outlook.co.th>
2016-03-18 05:27:23 +0700

    Update 2016-03-18 05:27

---
+++ application/modules/dashboard/controllers/ProductController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dashboard\Controllers;
+
+class ProductController extends CController {
+
+    public function initialize(){
+        parent::initialize();
+    }
+    
+    public function indexAction(){}
+    public function createAction(){}
+
+}
